0cbc79d425796881269e231178ba204b2c451efb
Onyango <jalochfelix@gmail.com>
2024-05-02 12:19:35 +0300

    add logic for comparing computer and human input

--- index.js
+++ index.js
@@ -7,23 +7,39 @@ function getComputerChoice () {
 
 var choice = getComputerChoice();
 
-if (choice <= 3) {
-    console.log( "Rock.");
-} else if (choice > 3 && choice <=6){
-    console.log("Paper.")
-} else {
-    console.log( "Scissors.");
+function equate (){
+    if (choice <= 3) {
+        return "Rock"
+    } else if (choice > 3 && choice <=6){
+       return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
 
+let compChoice = equate();
+console.log(compChoice)
 
+// prompt user to make a choice
 function getHumanChoice () {
-  let humanChoice = prompt("Rock, Paper, Scissors?");
-  return  humanChoice;
+   let choice = prompt("Rock, Paper, Scissors?");
+
+   return choice
+
   
+ }
+let hChoice = getHumanChoice();
+
+//  console.log(`The human choice is ${hChoice}`);
+
+// compare computer and human choice
+
+if (compChoice.toLowerCase() === "rock" && hChoice.toLowerCase() === "scissors" || compChoice.toLowerCase() === "paper" && hChoice.toLowerCase() === "rock" || compChoice.toLowerCase() === "scissors" && hChoice.toLowerCase() === "paper"){
+    console.log(`Computer wins! because you choose ${hChoice} and the computer choose ${compChoice}.`);
+} else if (hChoice.toLowerCase() === "rock" && compChoice.toLowerCase() === "scissors" || hChoice.toLowerCase() === "paper" && compChoice.toLowerCase() === "rock" || hChoice.toLowerCase() === "scissors" && compChoice.toLowerCase() === "paper"){
+    console.log(`You win! because you choose ${hChoice} and the computer choose ${compChoice}`)
+} else if (hChoice.toLowerCase() === compChoice.toLowerCase()) {
+    console.log(`It's a tie! because you and the computer choose ${hChoice}.`);
+} else {
+    console.log("Please try again!")
 }
-
-var hChoice = getHumanChoice();
-
-
-
-
